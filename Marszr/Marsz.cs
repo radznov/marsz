dbfb8993a7cc8875b4035e288eab63550a019d59
Radek <radznov@gmail.com>
2015-05-17 16:53:24 +0200

    Inicjalizacja

---
+++ Marszr/Marsz.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Marszr
+{
+    class Marsz
+    {
+        private int liczbaKlientow;
+        private int pojemnoscPojazdu;
+        private int zasiegPojazdu;
+        private Double rozwiazanieOptymalne;
+        private class Punkt
+        {
+            private Double x;
+            private Double y;
+            private Double q; //waga
+
+           public Punkt(Double x, Double y, Double q)
+            {
+                this.x = x;
+                this.y = y;
+                this.q = q;
+            }
+
+           public Double getX()
+           {
+               return this.x;
+           }
+           public Double getY()
+           {
+               return this.y;
+           }
+           public Double getQ()
+           {
+               return this.q;
+           }
+
+        }
+        List<Punkt> przesylka;
+        Punkt wspMagazynu;
+ //.........................................................................................................................
+
+        public Marsz(String nazwaPliku)
+        {
+            String[] linie = System.IO.File.ReadAllLines(nazwaPliku);
+
+            var tabInf = linie[0].Split(new String[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            this.liczbaKlientow = Int32.Parse(tabInf[0]);
+            this.pojemnoscPojazdu = Int32.Parse(tabInf[1]);
+            this.zasiegPojazdu = Int32.Parse(tabInf[2]);
+            //this.rozwiazanieOptymalne = Double.Parse(tabInf[4].Replace(".",","));
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+            this.rozwiazanieOptymalne = Double.Parse(tabInf[4]);
+
+            this.przesylka = new List<Punkt>();
+
+           // System.Console.Out.WriteLine(this.liczbaKlientow + " " + this.pojemnoscPojazdu + " " + this.zasiegPojazdu + " " + this.rozwiazanieOptymalne );
+
+            tabInf = linie[1].Split(new String[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            this.wspMagazynu = new Punkt(Double.Parse(tabInf[0]), Double.Parse(tabInf[1]), Double.Parse(tabInf[2]));
+
+            for (int i = 2; i < linie.Length; ++i)
+            {
+                var tab = linie[i].Split(new String[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+               this.przesylka.Add(new Punkt(Double.Parse(tab[0]), Double.Parse(tab[1]), Double.Parse(tab[2])));
+               System.Console.Out.WriteLine(Double.Parse(tab[0]) + " " + Double.Parse(tab[1]) + " " + Double.Parse(tab[2]));
+
+            }
+        }
+
+
+        public int mrowka(int[,] odleglosc, int ilosc_miast, int ilosc_tur, double bazowy_feromon, double mnoznik_feromonu, float wsp_parowania, float wsp_mrowek) // odleglosc[,] - macierz sasiedztwa grafu, ilosc_miast - ilosc miast :D, rozwiazanie zwracane jako wynik wywolania
+        {
+            //ilosc tur w ktorych "kazda z mrowek wraca po jedzenie", czyli ile razy wypuszczamy mrowki
+            const float alfa = 1.0F, beta = 3.0F; // alfa/beta (okreslaja parametry wyboru kolejnego miasta na trasie, "podobno" alfa najlepiej  = 1, beta <2,5>
+            const int wartosc_pewna = 10000; // ogolnie 1 stanowi wartosc pewna, ze wzgledu na losowanie wartosci calkowitych - u nas ta jedynka bedzie 10 000 (mozliwa zmiana)
+            int rozwiazanie = 99999999;
+            double[,] feromon = new double[ilosc_miast, ilosc_miast]; //utworzenie tablicy przechowujacej ilosci feromonu na danej sciezce
+            double[,] feromon_delta = new double[ilosc_miast, ilosc_miast]; //tablica z iloscia nowego feromonu, ktora dodajemy po powrocie wszystkich mroewk (dajmy te same warunki kazdej mrowce!)
+
+            int wartosc_odniesienia_feromonu = 0; //wartosc odniesienia, aby wiedziec mniej wiecej ile feromonu dodawac
+
+            for (int i = 0; i < ilosc_miast; i++)  //inicjalizacja wszystkich sciezek bazowa iloscia feromonu
+            {
+                for (int j = 0; j < ilosc_miast; j++)
+                {
+                    feromon[i, j] = bazowy_feromon; //a tu wartosc feromonu
+                    //Console.WriteLine("I: " + i + " J: " + j + " wartosc: " + odleglosc[i, j]);
+                    if (wartosc_odniesienia_feromonu < odleglosc[i, j] && i != j)
+                    {
+                        wartosc_odniesienia_feromonu = odleglosc[i, j];
+                    }
+                }
+            }
+
+            wartosc_odniesienia_feromonu *= ilosc_miast; //w zasadzie najgorsza mozliwa (w sumie to i prawie niemozliwa) dlogosc trasy
+
+            int[] dostepne_miasta = new int[ilosc_miast]; // tablica w ktorej bedziemy trzymac miasta dostepne do odwiedzenia dla mrowki
+            int[] najlepsza_trasa = new int[ilosc_miast]; //i zapamietanie najlepszej trasy
+            int ilosc_mrowek = (int)((float)ilosc_miast * wsp_mrowek); // domyslnie liczba mrowek rowna ilosci miast (podobno jest to optymalne)
+
+            //###########################################################################################################################################
+            for (int x = 0; x < ilosc_tur; x++) //glowna petla programowa
+            {
+                for (int i = 0; i < ilosc_miast; i++)  //za kazdym razem czyscimy tablice z nowa iloscia feromonu do dodania
+                {
+                    for (int j = 0; j < ilosc_miast; j++)
+                    {
+                        feromon_delta[i, j] = 0;
+                    }
+                }
+                //==========================================================================================================================================
+                Random rand = new Random();
+                for (int y = 0; y < ilosc_mrowek; y++) //dajemy kazdej mrowce przejsc sie po miastach
+                {
+                    int miasto_startowe = rand.Next(0, ilosc_miast - 1); //losowanie miasta startowe
+                    int IDmiast = ilosc_miast; //TO NIE ID tylko IloscDostepnychmiast! :)
+                    int obecne_miasto = miasto_startowe, kolejne_miasto = -1;
+                    for (int i = 0; i < ilosc_miast; i++) //wstepna inicjalizacja dostepnych miast
+                    {
+                        dostepne_miasta[i] = i;
+                    }
+                    int tmp; // zmienna do ustalenia kolejnosci (w sensie zamieniamy przy jej pomocy wartosci w tablicy - zwykle swap)
+                    tmp = dostepne_miasta[miasto_startowe];
+                    dostepne_miasta[miasto_startowe] = IDmiast - 1; // wyrzucenie miasta, do ktorego i tak wrocimy z listy (pod wyrzuceniem, rozumiem przeniesienia na koniec tablicy, do indeksow, do ktorych sie nie bedziemy odwolywac)
+                    dostepne_miasta[IDmiast - 1] = tmp;
+                    IDmiast--; // jedno miasto pooszloooo (krok wyzej)
+
+                    int dlugosc_trasy = 0; //dlugosc trasy wyliczona dla tej mrowki
+                    //...........................................................................................................................................
+                    for (int z = 0; z < ilosc_miast - 1; z++) // tutaj patrzymy jaka trase sobie wybrala 
+                    {
+                        double suma_we_wzorze = 0; //nazwa mowi za siebie
+                        int wybrana_droga_prd = 0, suma_prawdopodobienstwa = 0; //wybrana wartosc prawdopodobienstwa
+                        double[] prawdopodobienstwo = new double[IDmiast]; //tablica z prawdopodobienstwami wyboru kolejnego miasta
+
+                        for (int i = 0; i < IDmiast; i++) //obliczenie jednej ze zmiennych potrzebnej do wybrania kolejnego miasta
+                        {
+                            suma_we_wzorze += (Math.Pow(feromon[obecne_miasto, dostepne_miasta[i]], alfa) * (1.0 / Math.Pow(odleglosc[obecne_miasto, dostepne_miasta[i]], beta)));
+                        }
+
+                        for (int i = 0; i < IDmiast; i++) //liczenie prawdopodobienstwa wyboru dla kazdego z miast (taki ladny wzor)
+                        {
+                            prawdopodobienstwo[i] = ((Math.Pow(feromon[obecne_miasto, dostepne_miasta[i]], alfa) * ((double)1 / (double)Math.Pow(odleglosc[obecne_miasto, dostepne_miasta[i]], beta))) / (double)(suma_we_wzorze)) * wartosc_pewna;
+                            suma_prawdopodobienstwa += (int)prawdopodobienstwo[i];
+                        }
+
+                        wybrana_droga_prd = rand.Next(0, suma_prawdopodobienstwa - 1); // wybieramy liczbe z naszego przedzialu wartosci	
+
+                        suma_prawdopodobienstwa = 0; //wykorzystamy juz istniejaca zmienna
+                        for (int i = 0; i < IDmiast; i++)
+                        {
+                            suma_prawdopodobienstwa += (int)prawdopodobienstwo[i]; //dodajemy i czekamy, az przekroczymy
+                            if (suma_prawdopodobienstwa >= wybrana_droga_prd) //jezeli doszlismy do poszukiwnego prawdopodobienstwa
+                            {
+                                kolejne_miasto = dostepne_miasta[i];
+                                tmp = dostepne_miasta[i];
+                                dostepne_miasta[i] = dostepne_miasta[IDmiast - 1]; //zmniejszenie ilosc miast
+                                dostepne_miasta[IDmiast - 1] = tmp;
+                                break;
+                            }
+                        }
+
+                        if (kolejne_miasto == -1)
+                        {
+                            System.Console.Out.WriteLine("Nie wybrano kolejnego miasta!!");
+                            System.Console.In.Read();
+                        }
+                        else
+                        {
+                            dlugosc_trasy += odleglosc[obecne_miasto, kolejne_miasto];
+                        }
+                        obecne_miasto = kolejne_miasto;
+
+                        IDmiast--;
+                        kolejne_miasto = -1;
+                    }
+
+                    dlugosc_trasy += odleglosc[dostepne_miasta[0], miasto_startowe]; // i dodanie dlugosci drogi powrotnej do punktu
+
+                    if (rozwiazanie > dlugosc_trasy) //zapamietanie najlepszej trasy do tej pory
+                    {
+                        rozwiazanie = dlugosc_trasy;
+
+                        for (int i = ilosc_miast - 1; i >= 0; i--)
+                        {
+                            najlepsza_trasa[i] = dostepne_miasta[i];
+                        }
+                    }
+                    //...........................................................................................................................................
+                    float dodatek_feromonu = (int)((1.0 / dlugosc_trasy) * wartosc_odniesienia_feromonu * mnoznik_feromonu); //okreslamy ile feromonu dodajemy
+
+                    for (int i = ilosc_miast - 1; i > 0; i--) // dodanie feromonu do tablicy
+                    {
+                        feromon_delta[dostepne_miasta[i], dostepne_miasta[i - 1]] += dodatek_feromonu;
+                    }
+                    feromon_delta[dostepne_miasta[0], miasto_startowe] += dodatek_feromonu;
+
+                    for (int i = ilosc_miast - 1; i > 0; i--) //wyroznienie dodatkowo najlepszej trasy
+                    {
+                        feromon_delta[najlepsza_trasa[i], najlepsza_trasa[i - 1]] += dodatek_feromonu;
+                    }
+                    feromon_delta[najlepsza_trasa[0], najlepsza_trasa[ilosc_miast - 1]] += dodatek_feromonu;
+
+                }
+                //============================================================================================================================================
+                for (int i = 0; i < ilosc_miast; i++)  //parowanie feromonu i dodanie nowego od mrowek
+                {
+                    for (int j = 0; j < ilosc_miast; j++)
+                    {
+                        feromon[i, j] *= (1 - wsp_parowania); //szybkosc parowania
+                        feromon[i, j] += feromon_delta[i, j];
+                    }
+                }
+            }
+
+            return rozwiazanie;
+        }
+
+
+    }
+}
---
+++ Marszr/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marszr
+{
+    class Program
+    {
+
+        int wczytaj(string nazwa_pliku, int [,] odleglosc) // wczytanie pliku z danymi
+        {
+            string[] lines = System.IO.File.ReadAllLines(nazwa_pliku);
+
+            int ilosc_miast = Int32.Parse(lines[0]);
+            odleglosc = new int[ilosc_miast, ilosc_miast]; //utworzenie dwuwymiarowej dynamicznej tablicy(wiersze), na podstawie odczytanych danych
+
+
+            for (int i = 1; i < lines.Length; ++i)
+            {
+                var array = lines[i].Split(new string[]{" "}, StringSplitOptions.RemoveEmptyEntries);
+                for (int j = 0; j < array.Length; j++)
+                {
+                    //Console.WriteLine("I: " + i + " J: " + j + " wartosc: " + Int32.Parse(array[j]));
+                    odleglosc[i-1,j] = Int32.Parse(array[j]); // wczytywanie kolejnych wartosci do odpowiednich komorek tablicy
+                   
+                    if(odleglosc[i-1,j] == 0) // taak.. oszukanie, dla mrowkowego, zeby przez zero nie dzielic ;/ ale co zrobic.. bedzie wiekszy blad i tyle
+                    {
+                        odleglosc[i-1,j] = 1;
+                    }
+			
+                }
+            }
+            Console.WriteLine("\n\nPomyslnie odczytano dane z pliku.");
+            return ilosc_miast;
+        } 
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            Marsz marsz = new Marsz("ins.txt");
+	        string nazwa_pliku, wiersz, out_s; 
+	        int ile_razy; // ile razy kazdy z testow bedzie wykonywany
+            System.Console.In.ReadLine();
+	        System.Console.Clear();
+
+            System.Console.Out.WriteLine("nazwa pliku: ");
+            nazwa_pliku = System.Console.In.ReadLine();
+	        System.Console.Clear();
+
+	        System.Console.Out.WriteLine("Do jakiego pliku zapisac wynik? (bez rozszerzenia): ");
+            out_s = System.Console.In.ReadLine();
+	        System.Console.Clear();
+
+	        System.Console.Out.WriteLine("Ile razy wykonywac pomiary dla zadanej konfiguracji? ");
+            ile_razy =Convert.ToInt32(System.Console.In.ReadLine());
+	        System.Console.Clear();
+	
+
+
+		        int ilosc_miast = 0;
+
+		        int [] ilosc_tur = new int [1]; // dodaj pozniej bo brak
+                ilosc_tur[0] = 100;
+		        int bazowa_ilosc_feromonu = 1000;
+		        int mnoznik_dodatkowego_feromonu = 1;
+		        float [] wsp_parowania = new float [1];
+                wsp_parowania[0] = 0.4F;
+		        float [] wsp_mrowek = new float [1];
+                wsp_mrowek[0] = 0.4F;
+
+		        double mx = (wsp_mrowek.Length) * (wsp_parowania.Length) * ((double)ilosc_tur.Length) * ile_razy , sredni_czas = 0.0, sredni_wynik = 0.0, najlepszy = 9999999999.999, najgorszy = 0.0;
+
+                string[] lines = System.IO.File.ReadAllLines(nazwa_pliku);
+
+                ilosc_miast = Int32.Parse(lines[0]);
+                int[,] odleglosc = new int[ilosc_miast, ilosc_miast]; //utworzenie dwuwymiarowej dynamicznej tablicy(wiersze), na podstawie odczytanych danych
+
+
+                for (int i = 1; i < lines.Length; ++i)
+                {
+                    var array = lines[i].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        //Console.WriteLine("I: " + i + " J: " + j + " wartosc: " + Int32.Parse(array[j]));
+                        odleglosc[i - 1, j] = Int32.Parse(array[j]); // wczytywanie kolejnych wartosci do odpowiednich komorek tablicy
+
+                        if (odleglosc[i - 1, j] == 0) // taak.. oszukanie, dla mrowkowego, zeby przez zero nie dzielic ;/ ale co zrobic.. bedzie wiekszy blad i tyle
+                        {
+                            odleglosc[i - 1, j] = 1;
+                        }
+
+                    }
+                }
+                Console.WriteLine("\n\nPomyslnie odczytano dane z pliku.");
+                
+		        
+			        System.IO.StreamWriter wynik = new System.IO.StreamWriter(out_s + ".txt");
+			        
+
+			        //printf("Postep algorytmu: 00%%");
+
+
+			        for (int i = 0; i < ilosc_tur.Length ; i++)
+			        {
+
+				        wynik.WriteLine("===================================================================================================");
+                        wynik.WriteLine("Ilosc tur: ");
+                        wynik.WriteLine(ilosc_tur[i]);
+
+				        for(int k = 0 ; k < (wsp_parowania.Length) ; k++)
+				        {
+			              wynik.WriteLine("___________________________________________________________________________________________________");
+				          wynik.WriteLine("Wspolczynnik szybkosci parowania: ");
+				          wynik.WriteLine(wsp_parowania[k]);
+
+				        for(int l = 0 ; l < (wsp_mrowek.Length) ; l++)
+				        {
+			              wynik.WriteLine("....................................................................................................");
+				          wynik.WriteLine("Wspolczynnik ilosci mrowek do ilosci miast: ");
+				          wynik.WriteLine(wsp_mrowek[l]);
+				        for (int j = 0; j < ile_razy; j++)
+				        {
+					        int rozwiazanie = 9999999; //raczej nie bedzie gorsze od tego
+
+ 					        rozwiazanie = marsz.mrowka(odleglosc, ilosc_miast, ilosc_tur[i], bazowa_ilosc_feromonu, mnoznik_dodatkowego_feromonu, wsp_parowania[k], wsp_mrowek[l]);
+
+					        if(rozwiazanie < najlepszy)
+					        {
+						        najlepszy = rozwiazanie;
+					        }
+					        if(rozwiazanie > najgorszy)
+					        {
+						        najgorszy = rozwiazanie;
+					        }
+				        }
+
+
+				        System.Console.Out.WriteLine("\n\nIlosc miast: " + ilosc_miast);
+				        System.Console.Out.WriteLine("Bazowa wartosc feromonu: " + bazowa_ilosc_feromonu);
+				        System.Console.Out.WriteLine("Ilosc powtorzen kazdego z pomiarow: " + ile_razy);
+				        System.Console.Out.WriteLine("Najkrotsza odleglosc: " + najlepszy);
+				        System.Console.Out.WriteLine("Najgorsza odleglosc: " + najgorszy);
+
+                        wynik.WriteLine("\n\nIlosc miast: " + ilosc_miast);
+                        wynik.WriteLine("Bazowa wartosc feromonu: " + bazowa_ilosc_feromonu);
+                        wynik.WriteLine("Ilosc powtorzen kazdego z pomiarow: " + ile_razy);
+
+                        wynik.WriteLine("Najkrotsza odleglosc: " + najlepszy);
+                        wynik.WriteLine("Najgorsza odleglosc: " + najgorszy + "\n\n");
+
+				        najlepszy =  9999999999.999;
+				        najgorszy = 0.0;
+			        }
+			        }
+			        }
+                    System.Console.Out.WriteLine("\n\nZakonczono obliczenia!\n\n");
+			        wynik.Close(); // zamkniecie otwartego pliku
+
+
+		
+	        System.Console.In.Read();
+        }
+    }
+}
